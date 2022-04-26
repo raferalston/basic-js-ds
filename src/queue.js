183297f103e94a71ac4ac133ac46c109f53194c8
raferalston <like2pwnyou@yandex.ru>
2022-04-26 09:44:43 +0300

    feat: done

--- src/queue.js
+++ src/queue.js
@@ -14,19 +14,31 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.q = []
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.q.reverse().reduce((acc, cur) => {
+      if (acc) {
+        const node = new ListNode(cur);
+        node.next = acc;
+        return node;
+      }
+  
+      return new ListNode(cur);
+    }, null);
   }
 
   enqueue(value) {
-    throw new NotImplementedError('Not implemented');
+    this.q.push(value)
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    return this.q.shift()
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
